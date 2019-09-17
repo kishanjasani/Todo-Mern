8f2f6026b56893ea3006e4dbebe2a6ca2c977c9c
Kishan Jasani <kishanjasani007@yahoo.in>
2019-09-17 15:03:36 +0530

    Create simple react app with form element for todo lists

---
+++ src/components/edit-todo.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p>Edit Todo</p>
+      </div>
+    );
+  }
+}
+
+export default EditTodo;
